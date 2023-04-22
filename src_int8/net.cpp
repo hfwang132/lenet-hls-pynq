7c7142e756920414032031f39f219c38e995ce01
Haifei Wang <godfly66666@gmail.com>
2023-04-22 16:38:32 +0800

    Replace `TEST` with `DEBUG`

--- src_int8/net.cpp
+++ src_int8/net.cpp
@@ -1,9 +1,9 @@
 #include "op.h"
 #include "param.h"
 
-//#define TEST
+//#define DEBUG
 
-#ifdef TEST
+#ifdef DEBUG
 #include "tbutils.h"
 #endif
 
@@ -17,7 +17,7 @@ int lenet(INPUT (&img)[32][32])
 	INPUT img_preprocessed[1][32][32] = {0};
     preprocess(img, img_preprocessed);
 
-#ifdef TEST
+#ifdef DEBUG
     arr2txt("img_preprocessed_0.txt", img_preprocessed[0]); //
 #endif
 
@@ -28,7 +28,7 @@ int lenet(INPUT (&img)[32][32])
     conv_relu(img_preprocessed, conv1_weights, conv1_bias, conv1_relu_rst);
     scale(conv1_relu_rst, n_conv1, m_conv1, pool1_in);
 
-#ifdef TEST
+#ifdef DEBUG
     arr2txt("conv1_relu_rst_0.txt", conv1_relu_rst[0]);
 #endif
 
@@ -42,7 +42,7 @@ int lenet(INPUT (&img)[32][32])
     conv_relu(pool1_rst, conv2_weights, conv2_bias, conv2_relu_rst);
     scale(conv2_relu_rst, n_conv2, m_conv2, pool2_in);
 
-#ifdef TEST
+#ifdef DEBUG
     arr2txt("conv2_relu_rst_0.txt", conv2_relu_rst[0]);
 #endif
     // (4) pool2
@@ -59,7 +59,7 @@ int lenet(INPUT (&img)[32][32])
     fc_relu(flattened, fc1_weights, fc1_bias, fc1_relu_rst);
     scale(fc1_relu_rst, n_fc1, m_fc1, fc2_in);
 
-#ifdef TEST
+#ifdef DEBUG
     arr1d2txt("fc1_relu_rst.txt", fc1_relu_rst);
 #endif
 
@@ -69,7 +69,7 @@ int lenet(INPUT (&img)[32][32])
     fc_relu(fc2_in, fc2_weights, fc2_bias, fc2_relu_rst);
     scale(fc2_relu_rst, n_fc2, m_fc2, fc3_in);
 
-#ifdef TEST
+#ifdef DEBUG
     arr1d2txt("fc2_relu_rst.txt", fc2_relu_rst);
 #endif
 
@@ -77,7 +77,7 @@ int lenet(INPUT (&img)[32][32])
     BIAS final_rst[10] = {0};
     fc(fc3_in, fc3_weights, fc3_bias, final_rst);
 
-#ifdef TEST
+#ifdef DEBUG
     arr1d2txt("final_rst.txt", final_rst);
 #endif
 
